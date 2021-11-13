b2c7e1712db3b3f839c0fc60434a88ba12e64719
0aleksandar0 <92309291+0aleksandar0@users.noreply.github.com>
2021-11-14 00:10:47 +0100

    start-chat-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let's built chat-app!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsgCJijI6zfLN98zNc9-cszr_pI6UxxvQ",
+  authDomain: "asu-chatapp.firebaseapp.com",
+  projectId: "asu-chatapp",
+  storageBucket: "asu-chatapp.appspot.com",
+  messagingSenderId: "583207781404",
+  appId: "1:583207781404:web:36ff79e8e717c3ab7b8bd9",
+};
