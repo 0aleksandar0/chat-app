ba0ab47b7aa0fe043dfdf7fdd41767dc7fcf9576
0aleksandar0 <92309291+0aleksandar0@users.noreply.github.com>
2021-11-14 00:26:41 +0100

    update-chat-app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import React from "react";
+import Sidebar from "./components/Sidebar";
+import { Chat } from "./components/Chat";
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's built chat-app!</h1>
+      <Sidebar />
+      <Chat />
     </div>
   );
 }
---
+++ src/components/Chat.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../Chat.css";
+
+export const Chat = () => {
+  return (
+    <div className="chat">
+      <h1>I am the chat</h1>
+    </div>
+  );
+};
