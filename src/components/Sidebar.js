eddb4566e8c6fbcb6ffbf62f7fe4a9d551d83961
0aleksandar0 <92309291+0aleksandar0@users.noreply.github.com>
2021-11-16 23:18:56 +0100

    Sidebar update
    Styling the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,39 @@
 import React from "react";
 import "../Sidebar.css";
 import { DonutLarge } from "@mui/icons-material";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Avatar, IconButton } from "@mui/material";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import SidebarChat from "./SidebarChat";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
+        <Avatar src="../images/me.jpg" />
         <div className="sidebar__headerRight">
-          <DonutLarge />
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
